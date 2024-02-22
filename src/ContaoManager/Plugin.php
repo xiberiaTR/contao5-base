9ce757dee674835d464dff5906e0217c6ec4f7b0
xiberiaTR <156435643+xiberiaTR@users.noreply.github.com>
2024-02-22 16:51:25 +0100

    Update Plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -2,20 +2,20 @@
 
 declare (strict_types = 1);
 
-namespace trdev\ContaoBaseBundle\ContaoManager;
+namespace trdev\ContaoAmaltisBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use trdev\ContaoBaseBundle\ContaoBaseBundle;
+use trdev\ContaoAmaltisBundle\ContaoAmaltisBundle;
 
 class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create(ContaoBaseBundle::class)
+            BundleConfig::create(ContaoAmaltisBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
