a0a1f93f4898d0aaccdd473eb6297bdae1567696
xiberiaTR <156435643+xiberiaTR@users.noreply.github.com>
2024-02-22 16:50:58 +0100

    Update and rename ContaoBaseBundle.php to ContaoAmaltisBundle.php

--- src/ContaoAmaltisBundle.php
+++ src/ContaoAmaltisBundle.php
@@ -2,10 +2,10 @@
 
 declare (strict_types = 1);
 
-namespace trdev\ContaoBaseBundle;
+namespace trdev\ContaoAmaltisBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ContaoBaseBundle extends Bundle
+class ContaoAmaltisBundle extends Bundle
 {
 }
